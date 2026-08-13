f3612c66acdb8c5d4e3172ee8ba57d9cb0a84ec0
Juan Pablo Cano Cuadros <jccuadro@bancolombia.com.co>
2026-08-13 18:50:23 -0500

    validar funcionamiento

---
+++ farmacia_v1/src/main/java/com/farmacia/taller/v1/service/MedicamentoServiceTest.java
@@ -0,0 +1,57 @@
+package com.farmacia.taller.v1.service;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import com.farmacia.taller.v1.dto.MedicamentoResponseDTO;
+import com.farmacia.taller.v1.model.Medicamento;
+import com.farmacia.taller.v1.repository.MedicamentoRepository;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.when;
+import java.util.Optional;
+public class MedicamentoServiceTest {
+
+    @Mock
+    private MedicamentoRepository medicamentoRepository;
+
+    @InjectMocks
+    private MedicamentoService medicamentoService;
+    //mock
+
+    /// inyeccion de los mocks
+    @Test
+    void obtenerPorId_rutaFeliz() {
+        //arrange
+        Long idTest = 1L;
+        Medicamento medicamentoSimulado = new Medicamento();
+        medicamentoSimulado.setId(idTest);
+        medicamentoSimulado.setNombre("Paracetamol");
+        medicamentoSimulado.setPrecio(5.0);
+
+        when(medicamentoRepository.findById(idTest)).thenReturn(Optional.of(medicamentoSimulado));
+
+        //act
+        MedicamentoResponseDTO resultado = medicamentoService.obtenerPorId(idTest);
+
+
+        // Assert
+        // assertNotNull(resultado);
+        assertEquals(idTest, resultado.getId());
+        assertEquals("Paracetamol", resultado.getNombre());
+        assertEquals(5.0, resultado.getPrecio());
+
+        //verify
+
+
+
+
+        
+    }
+
+
+    
+}
